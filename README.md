962115b7cda75f8c1069a4337f12ff2fd69f7726
Ruan Brito <131319211+BritoRuan@users.noreply.github.com>
2025-10-27 21:00:47 +0000

    chore: adding scripts `lint:check` and `lint:fix`

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # clone-tabnews
+
 implementation of https://www.tabnews.com.br/ for the course.dev
--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,11 @@
 function Home() {
-  return <h1>Não se limite a se preparar para o amanhã. Procure também descartar-se daquilo que já não faz mais sentido, que não é produtivo, que não contribui para os objetivos. - Peter Drucker</h1>
+  return (
+    <h1>
+      Não se limite a se preparar para o amanhã. Procure também descartar-se
+      daquilo que já não faz mais sentido, que não é produtivo, que não
+      contribui para os objetivos. - Peter Drucker
+    </h1>
+  );
 }
 
 export default Home;
