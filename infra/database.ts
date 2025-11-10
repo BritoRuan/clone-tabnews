53e2a40d6b8767c58b0cff02f88063fff95cc1c7
BritoRuan <ruan.desenvolvedor@gmail.com>
2025-11-10 08:36:10 -0300

    fix: makes `database.ts` `client.connect()` more robust to erros

--- infra/database.ts
+++ infra/database.ts
@@ -8,13 +8,23 @@ async function query(queryObject: string | QueryConfig): Promise<QueryResult> {
     database: process.env.POSTGRES_DB,
     password: process.env.POSTGRES_PASSWORD,
   });
-  await client.connect();
+
+  console.log('Credencias do postgres', {
+    host: process.env.POSTGRES_HOST,
+    port: parseInt(process.env.POSTGRES_PORT),
+    user: process.env.POSTGRES_USER,
+    database: process.env.POSTGRES_DB,
+    password: process.env.POSTGRES_PASSWORD,
+  })
+
 
   try {
+    await client.connect();
     const result = await client.query(queryObject);
     return result;
   } catch (error) {
     console.error(error);
+    throw error;
   } finally {
     await client.end();
   }
