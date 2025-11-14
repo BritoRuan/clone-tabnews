e61300486dcb073c6df0c0cd7bbbce7f8c558751
BritoRuan <ruan.desenvolvedor@gmail.com>
2025-11-13 21:32:17 -0300

    feat: add database query functionality and default migration options

---
+++ infra/database/migrations/default-migration-options.ts
@@ -0,0 +1,11 @@
+import { RunnerOption } from "node-pg-migrate";
+import { join } from "node:path";
+
+export const defaultMigrationOptions: RunnerOption = {
+  databaseUrl: process.env.DATABASE_URL,
+  dryRun: true,
+  dir: join(process.cwd(), "dist", "infra", "migrations"),
+  direction: "up",
+  verbose: true,
+  migrationsTable: process.env.DATABASE_MIGRATIONS_TABLE,
+};
