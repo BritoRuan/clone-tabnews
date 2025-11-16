39b4ca20b763b9873cc1c295e965146c2a0ce56d
BritoRuan <ruan.desenvolvedor@gmail.com>
2025-11-16 20:56:44 -0300

    fix: update migration directory path based on environment for improved deployment handling

--- infra/database/migrations/default-migration-options.ts
+++ infra/database/migrations/default-migration-options.ts
@@ -1,10 +1,14 @@
 import { RunnerOption } from "node-pg-migrate";
 import { join } from "node:path";
 
+const isProduction = process.env.NODE_ENV === "production";
+
 export const defaultMigrationOptions: RunnerOption = {
   databaseUrl: process.env.DATABASE_URL,
   dryRun: true,
-  dir: join(process.cwd(), "dist", "infra", "migrations"),
+  dir: isProduction
+    ? join(process.cwd(), ".next", "server", "infra", "migrations")
+    : join(process.cwd(), "infra", "migrations"),
   direction: "up",
   verbose: true,
   migrationsTable: process.env.DATABASE_MIGRATIONS_TABLE,
