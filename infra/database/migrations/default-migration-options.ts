0dcd4d7669504fd3d1c0b9c4e9bcd405813e5280
BritoRuan <ruan.desenvolvedor@gmail.com>
2025-11-17 18:37:19 -0300

    fix: update build script to simplify migration handling and remove unused scripts

--- infra/database/migrations/default-migration-options.ts
+++ infra/database/migrations/default-migration-options.ts
@@ -1,15 +1,10 @@
 import { RunnerOption } from "node-pg-migrate";
 import { join } from "node:path";
 
-const isProduction = process.env.NODE_ENV === "production";
-const migrationsDir = isProduction
-  ? join(process.cwd(), ".next", "server", "infra", "migrations")
-  : join(process.cwd(), "dist", "infra", "migrations");
-
 export const defaultMigrationOptions: RunnerOption = {
   databaseUrl: process.env.DATABASE_URL,
   dryRun: true,
-  dir: migrationsDir,
+  dir: join(process.cwd(), "dist", "infra", "migrations"),
   direction: "up",
   verbose: true,
   migrationsTable: process.env.DATABASE_MIGRATIONS_TABLE,
