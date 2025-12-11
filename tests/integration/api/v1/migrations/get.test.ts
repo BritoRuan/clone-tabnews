110b4538e23f30adf2d82533b87251f5e6bf12ce
BritoRuan <ruan.desenvolvedor@gmail.com>
2025-12-10 21:53:23 -0300

    test: rename all descriptions to use new pattern

--- tests/integration/api/v1/migrations/get.test.ts
+++ tests/integration/api/v1/migrations/get.test.ts
@@ -15,18 +15,19 @@ describe("GET /api/v1/migrations", () => {
   beforeAll(async () => {
     await resetDatabase();
   });
+  describe("Anonymous user", () => {
+    it("Running pending migrations", async () => {
+      const { req: request, res: response } = createMocks({
+        method: "GET",
+      });
 
-  test("should return 200 with a non-empty list of applied migrations", async () => {
-    const { req: request, res: response } = createMocks({
-      method: "GET",
+      await handler(request, response);
+
+      const responseBody = response._getJSONData();
+
+      expect(response._getStatusCode()).toBe(200);
+      expect(Array.isArray(responseBody)).toBe(true);
+      expect(responseBody.length).toBeGreaterThan(0);
     });
-
-    await handler(request, response);
-
-    const responseBody = response._getJSONData();
-
-    expect(response._getStatusCode()).toBe(200);
-    expect(Array.isArray(responseBody)).toBe(true);
-    expect(responseBody.length).toBeGreaterThan(0);
   });
 });
--- tests/integration/api/v1/migrations/post.test.ts
+++ tests/integration/api/v1/migrations/post.test.ts
@@ -16,29 +16,35 @@ describe("POST /api/v1/migrations", () => {
     await resetDatabase();
   });
 
-  test("should apply migrations on first call and return empty list on second call", async () => {
-    const { req: firstRequest, res: firstResponse } = createMocks({
-      method: "POST",
+  describe("Anonymous user", () => {
+    describe("Running pending migrations", () => {
+      it("For the first time", async () => {
+        const { req: firstRequest, res: firstResponse } = createMocks({
+          method: "POST",
+        });
+
+        await handler(firstRequest, firstResponse);
+
+        const firstResponseBody = firstResponse._getJSONData();
+
+        expect(firstResponse._getStatusCode()).toBe(201);
+        expect(Array.isArray(firstResponseBody)).toBe(true);
+        expect(firstResponseBody.length).toBeGreaterThan(0);
+      });
+
+      it("For the second time", async () => {
+        const { req: secondRequest, res: secondResponse } = createMocks({
+          method: "POST",
+        });
+
+        await handler(secondRequest, secondResponse);
+
+        const secondResponseBody = secondResponse._getJSONData();
+
+        expect(secondResponse._getStatusCode()).toBe(200);
+        expect(Array.isArray(secondResponseBody)).toBe(true);
+        expect(secondResponseBody.length).toBe(0);
+      });
     });
-
-    await handler(firstRequest, firstResponse);
-
-    const firstResponseBody = firstResponse._getJSONData();
-
-    expect(firstResponse._getStatusCode()).toBe(201);
-    expect(Array.isArray(firstResponseBody)).toBe(true);
-    expect(firstResponseBody.length).toBeGreaterThan(0);
-
-    const { req: secondRequest, res: secondResponse } = createMocks({
-      method: "POST",
-    });
-
-    await handler(secondRequest, secondResponse);
-
-    const secondResponseBody = secondResponse._getJSONData();
-
-    expect(secondResponse._getStatusCode()).toBe(200);
-    expect(Array.isArray(secondResponseBody)).toBe(true);
-    expect(secondResponseBody.length).toBe(0);
   });
 });
