3376ef275f94d1731584a06ba209059abe92b882
BritoRuan <ruan.desenvolvedor@gmail.com>
2025-12-16 22:40:31 -0300

    chore: update migration options and paths in configuration files

--- infra/database/migrations/default-migration-options.ts
+++ infra/database/migrations/default-migration-options.ts
@@ -3,7 +3,7 @@ import { RunnerOption } from "node-pg-migrate";
 export const defaultMigrationOptions: RunnerOption = {
   databaseUrl: process.env.DATABASE_URL,
   dryRun: false,
-  dir: process.env.MIGRATIONS_DIR,
+  dir: process.env.MIGRATIONS_DIR ?? "dist/infra/migrations",
   direction: "up",
   verbose: true,
   migrationsTable: process.env.DATABASE_MIGRATIONS_TABLE,
--- infra/migrations/1765932931851_test-migration-vercel.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.shorthands = void 0;
-exports.up = up;
-exports.down = down;
-exports.shorthands = undefined;
-async function up(pgm) {}
-async function down(pgm) {}
