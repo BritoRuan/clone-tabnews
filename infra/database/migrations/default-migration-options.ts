efd18b3b24e90c5e13017e291a1aaeb4beeae2d1
BritoRuan <ruan.desenvolvedor@gmail.com>
2025-12-16 21:51:15 -0300

    fix: update migration options for correct directory and dryRun setting

--- infra/database/migrations/default-migration-options.ts
+++ infra/database/migrations/default-migration-options.ts
@@ -3,8 +3,8 @@ import { resolve } from "node:path";
 
 export const defaultMigrationOptions: RunnerOption = {
   databaseUrl: process.env.DATABASE_URL,
-  dryRun: true,
-  dir: resolve(__dirname, "../../../infra/migrations"),
+  dryRun: false,
+  dir: resolve(__dirname, "../migrations"),
   direction: "up",
   verbose: true,
   migrationsTable: process.env.DATABASE_MIGRATIONS_TABLE,
