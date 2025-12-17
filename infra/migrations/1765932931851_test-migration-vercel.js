6f10a787577532d5d074a7ff8b3e4649d7aa7ae3
BritoRuan <ruan.desenvolvedor@gmail.com>
2025-12-16 22:26:49 -0300

    chore: update migration configurations and paths in environment and config files

---
+++ infra/migrations/1765932931851_test-migration-vercel.js
@@ -0,0 +1,8 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.shorthands = void 0;
+exports.up = up;
+exports.down = down;
+exports.shorthands = undefined;
+async function up(pgm) { }
+async function down(pgm) { }
