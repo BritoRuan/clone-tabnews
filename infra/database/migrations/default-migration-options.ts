1c172ad62c64b244c5a83a34bf66ef21afe89852
BritoRuan <ruan.desenvolvedor@gmail.com>
2025-12-16 22:51:54 -0300

    chore: update migration options and scripts for consistency and add ts-node dependency

--- infra/database/migrations/default-migration-options.ts
+++ infra/database/migrations/default-migration-options.ts
@@ -1,10 +1,11 @@
 import { RunnerOption } from "node-pg-migrate";
+import { join } from "node:path";
 
 export const defaultMigrationOptions: RunnerOption = {
   databaseUrl: process.env.DATABASE_URL,
   dryRun: false,
-  dir: process.env.MIGRATIONS_DIR ?? "dist/infra/migrations",
+  dir: join(process.cwd(), "infra", "migrations"),
   direction: "up",
   verbose: true,
-  migrationsTable: process.env.DATABASE_MIGRATIONS_TABLE,
+  migrationsTable: process.env.DATABASE_MIGRATIONS_TABLE ?? "pgmigrations",
 };
