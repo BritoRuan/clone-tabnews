b454b26bef842dcf4f528f14d16d172ce413b1bf
Ruan Brito <131319211+BritoRuan@users.noreply.github.com>
2025-12-17 21:35:55 -0300

    chore/maintenance (#21)
    * build: lock dependencies versions
    
    * build: update dependencies
    
    * refactor: rename `wait-for-postgres` script to `services:wait:database`
    
    * refactor: rename `migration` scripts to `migrations`
    
    * chore: add `.eslintignore` to be used ignore some folders and files
    
    * refactor: move `test` scripts to top
    
    * feat: add `posttest` script
    
    * chore: add `==verbose`to `test:watch`
    
    * test: rename all descriptions to use new pattern
    
    * refactor: use `orchestrator.clearDatabase()` in tests
    
    * fix: change `join` to `resolve` in `/api/v1/migrations`
    
    * fix: adjust absolute path to retrieving migrations
    
    * fix: update migration options for correct directory and dryRun setting
    
    * chore: update migration configurations and add new migration file
    
    * chore: update migration configurations and paths in environment and config files
    
    * chore: standardize import quotes in migration files
    
    * chore: update migration options and paths in configuration files
    
    * chore: update migration options and scripts for consistency and add ts-node dependency
    
    * refactor: update migration scripts and TypeScript configuration
    
    * chore: update migration directory resolution to use resolve instead of join
    
    * chore: add initial migration file for testettsts
    
    * chore: update build script to include migration build step
    
    * chore: remove obsolete migrations files and add new migration for vercel
    
    * chore: format migration file for consistency
    
    * chore: update migration directory path to use 'dist' for consistency
    
    * chore: update default migration options to use environment variable for directory path
    
    * chore: update migration command to include pattern for migration files

--- tests/integration/api/v1/migrations/get.test.ts
+++ tests/integration/api/v1/migrations/get.test.ts
@@ -1,32 +1,26 @@
-import database from "infra/database/database";
 import handler from "pages/api/v1/migrations";
 import { createMocks } from "node-mocks-http";
 import orchestrator from "@/tests/orchestrator";
 
-async function resetDatabase() {
-  await database.query("DROP SCHEMA public CASCADE; CREATE SCHEMA public;");
-}
-
 describe("GET /api/v1/migrations", () => {
   beforeAll(async () => {
     await orchestrator.waitForAllServices();
+    await orchestrator.clearDatabase();
   });
 
-  beforeAll(async () => {
-    await resetDatabase();
-  });
+  describe("Anonymous user", () => {
+    it("Running pending migrations", async () => {
+      const { req: request, res: response } = createMocks({
+        method: "GET",
+      });
 
-  test("should return 200 with a non-empty list of applied migrations", async () => {
-    const { req: request, res: response } = createMocks({
-      method: "GET",
+      await handler(request, response);
+
+      const responseBody = response._getJSONData();
+
+      expect(response._getStatusCode()).toBe(200);
+      expect(Array.isArray(responseBody)).toBe(true);
+      expect(responseBody.length).toBeGreaterThan(0);
     });
-
-    await handler(request, response);
-
-    const responseBody = response._getJSONData();
-
-    expect(response._getStatusCode()).toBe(200);
-    expect(Array.isArray(responseBody)).toBe(true);
-    expect(responseBody.length).toBeGreaterThan(0);
   });
 });
--- tests/integration/api/v1/migrations/post.test.ts
+++ tests/integration/api/v1/migrations/post.test.ts
@@ -1,44 +1,42 @@
 import orchestrator from "@/tests/orchestrator";
-import database from "infra/database/database";
 import { createMocks } from "node-mocks-http";
 import handler from "pages/api/v1/migrations";
-
-async function resetDatabase() {
-  await database.query("DROP SCHEMA public CASCADE; CREATE SCHEMA public;");
-}
 
 describe("POST /api/v1/migrations", () => {
   beforeAll(async () => {
     await orchestrator.waitForAllServices();
+    await orchestrator.clearDatabase();
   });
 
-  beforeAll(async () => {
-    await resetDatabase();
-  });
+  describe("Anonymous user", () => {
+    describe("Retrieving pending migrations", () => {
+      it("For the first time", async () => {
+        const { req: firstRequest, res: firstResponse } = createMocks({
+          method: "POST",
+        });
 
-  test("should apply migrations on first call and return empty list on second call", async () => {
-    const { req: firstRequest, res: firstResponse } = createMocks({
-      method: "POST",
+        await handler(firstRequest, firstResponse);
+
+        const firstResponseBody = firstResponse._getJSONData();
+
+        expect(firstResponse._getStatusCode()).toBe(201);
+        expect(Array.isArray(firstResponseBody)).toBe(true);
+        expect(firstResponseBody.length).toBeGreaterThan(0);
+      });
+
+      it("For the second time", async () => {
+        const { req: secondRequest, res: secondResponse } = createMocks({
+          method: "POST",
+        });
+
+        await handler(secondRequest, secondResponse);
+
+        const secondResponseBody = secondResponse._getJSONData();
+
+        expect(secondResponse._getStatusCode()).toBe(200);
+        expect(Array.isArray(secondResponseBody)).toBe(true);
+        expect(secondResponseBody.length).toBe(0);
+      });
     });
-
-    await handler(firstRequest, firstResponse);
-
-    const firstResponseBody = firstResponse._getJSONData();
-
-    expect(firstResponse._getStatusCode()).toBe(201);
-    expect(Array.isArray(firstResponseBody)).toBe(true);
-    expect(firstResponseBody.length).toBeGreaterThan(0);
-
-    const { req: secondRequest, res: secondResponse } = createMocks({
-      method: "POST",
-    });
-
-    await handler(secondRequest, secondResponse);
-
-    const secondResponseBody = secondResponse._getJSONData();
-
-    expect(secondResponse._getStatusCode()).toBe(200);
-    expect(Array.isArray(secondResponseBody)).toBe(true);
-    expect(secondResponseBody.length).toBe(0);
   });
 });
