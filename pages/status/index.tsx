df0fc227b8231d26d19b83696bc841cf0dba9b98
BritoRuan <ruan.desenvolvedor@gmail.com>
2025-12-19 23:19:01 -0300

    feat: add status page with database informations

---
+++ pages/status/index.tsx
@@ -0,0 +1,75 @@
+import useSWR from "swr";
+
+type DatabaseInfo = {
+  version: string;
+  max_connections: number;
+  opened_connections: number;
+};
+
+type StatusApiResponse = {
+  updated_at: string;
+  dependencies: {
+    database: DatabaseInfo;
+  };
+};
+
+type StatusViewModel = {
+  updatedAt: string;
+  database: DatabaseInfo;
+};
+
+async function fetchStatus(endpoint: string): Promise<StatusViewModel> {
+  const response = await fetch(endpoint);
+  const data: StatusApiResponse = await response.json();
+
+  return {
+    updatedAt: data.updated_at,
+    database: data.dependencies.database,
+  };
+}
+
+function UpdatedAt() {
+  const response = useSWR("/api/v1/status", fetchStatus, {
+    refreshInterval: 2000,
+  });
+
+  if (response.isLoading || !response.data) {
+    return <p>Última atualização: carregando…</p>;
+  }
+
+  const updatedAtValueText = new Date(response.data.updatedAt).toLocaleString(
+    "pt-BR",
+  );
+  return <p>Última atualização: {updatedAtValueText}</p>;
+}
+
+const DatabaseStatusInfo = () => {
+  const response = useSWR("/api/v1/status", fetchStatus, {
+    refreshInterval: 2000,
+  });
+
+  if (response.isLoading || !response.data) {
+    return <p>Informações do Banco de dados: Carregando…</p>;
+  }
+
+  return (
+    <section>
+      <h2>Informações do Banco de dados</h2>
+      <ul>
+        <li>Versão: {response.data.database.version}</li>
+        <li>Conexões máximas: {response.data.database.max_connections}</li>
+        <li>Conexões abertas: {response.data.database.opened_connections}</li>
+      </ul>
+    </section>
+  );
+};
+
+export default function StatusPage() {
+  return (
+    <>
+      <h1>Status</h1>
+      <UpdatedAt />
+      <DatabaseStatusInfo />
+    </>
+  );
+}
