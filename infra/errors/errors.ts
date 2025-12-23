eff03f36c4aa1f80d3d7cf18485f7c0c468a83c8
BritoRuan <ruan.desenvolvedor@gmail.com>
2025-12-23 19:32:54 -0300

    feat: create and use InternalServerError in `/api/v1/status`

---
+++ infra/errors/errors.ts
@@ -0,0 +1,23 @@
+export class InternalServerError extends Error {
+  action: string;
+  statusCode: number;
+
+  constructor(options: ErrorOptions) {
+    super("Um erro interno não esperado aconteceu.", {
+      cause: options.cause,
+    });
+
+    this.name = "InternalServerError";
+    this.action = "Entre em contato com o suporte.";
+    this.statusCode = 500;
+  }
+
+  toJSON() {
+    return {
+      name: this.name,
+      message: this.message,
+      action: this.action,
+      statusCode: this.statusCode,
+    };
+  }
+}
