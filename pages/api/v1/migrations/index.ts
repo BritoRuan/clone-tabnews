2031fea67c38080d608237bc08e8b9aebeff846a
BritoRuan <ruan.desenvolvedor@gmail.com>
2026-01-10 00:23:21 -0300

    feat: add migrator model and remove logic from migrations controller

--- pages/api/v1/migrations/index.ts
+++ pages/api/v1/migrations/index.ts
@@ -1,9 +1,7 @@
 import { createRouter } from "next-connect";
-import { defaultMigrationOptions } from "@/infra/database/migrations/default-migration-options";
 import { NextApiRequest, NextApiResponse } from "next";
-import migrationRunner from "node-pg-migrate";
-import database from "@/infra/database/database";
 import controller from "@/infra/controllers/controllers";
+import migrator from "@/models/migrator";
 
 const router = createRouter<NextApiRequest, NextApiResponse>();
 router.get(getHandler);
@@ -12,38 +10,19 @@ router.post(postHandler);
 export default router.handler(controller.errorHandlers);
 
 async function getHandler(_request: NextApiRequest, response: NextApiResponse) {
-  const client = await database.getNewClient();
-  try {
-    const pendingMigrations = await migrationRunner({
-      ...defaultMigrationOptions,
-      noLock: true,
-      dbClient: client,
-    });
-
-    return response.status(200).json(pendingMigrations);
-  } finally {
-    client.end();
-  }
+  const pendingMigrations = await migrator.listPendingMigrations();
+  return response.status(200).json(pendingMigrations);
 }
 
 async function postHandler(
   _request: NextApiRequest,
   response: NextApiResponse,
 ) {
-  const client = await database.getNewClient();
+  const migratedMigrations = await migrator.runPendingMigrations();
 
-  try {
-    const migratedMigrations = await migrationRunner({
-      ...defaultMigrationOptions,
-      dryRun: false,
-      dbClient: client,
-    });
-
-    if (migratedMigrations.length > 0) {
-      return response.status(201).json(migratedMigrations);
-    }
-    return response.status(200).json(migratedMigrations);
-  } finally {
-    client.end();
+  if (migratedMigrations.length > 0) {
+    return response.status(201).json(migratedMigrations);
   }
+
+  return response.status(200).json(migratedMigrations);
 }
