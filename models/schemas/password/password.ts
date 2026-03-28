5f52e252b725a1ea04fb96e70ee86fd0f27bf472
Ruan Brito <131319211+BritoRuan@users.noreply.github.com>
2026-03-28 02:44:43 -0300

    feature/orchestrator-create-user (#42)
    * feat: add validate input field password
    
    * test: simplify integration tests with `createUser` helper
    
    * feat: add create user method to use in integration tests
    
    * chore: add lib faker-js to use in integration tests

--- models/schemas/password/password.ts
+++ models/schemas/password/password.ts
@@ -1,6 +1,8 @@
 import bcryptjs from "bcryptjs";
+import validatePassword from "@/models/validators/password/validate-input-field-password";
 
 async function hash(password: string) {
+  validatePassword(password);
   const rounds = getNumberOfRounds();
   const pepper = getPepper();
   return await bcryptjs.hash(password + pepper, rounds);
