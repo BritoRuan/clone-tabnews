bab32072f8f230acdaaf2111d8da3843cef2c2ea
BritoRuan <ruan.desenvolvedor@gmail.com>
2026-03-27 23:34:03 -0300

    feat: add patch user update endpoint and integration tests

--- pages/api/v1/users/[username]/index.ts
+++ pages/api/v1/users/[username]/index.ts
@@ -1,10 +1,12 @@
 import controller from "@/infra/controllers/controllers";
+import { ValidationError } from "@/infra/errors/ValidationError";
 import user from "@/models/schemas/users/user";
 import { NextApiRequest, NextApiResponse } from "next";
 import { createRouter } from "next-connect";
 
 const router = createRouter<NextApiRequest, NextApiResponse>();
 router.get(getHandler);
+router.patch(patchHandler);
 
 export default router.handler(controller.errorHandlers);
 
@@ -12,4 +14,26 @@ async function getHandler(request: NextApiRequest, response: NextApiResponse) {
   const username = request.query.username as string;
   const userFound = await user.findOneByUsername(username);
   return response.status(200).json(userFound);
+}
+
+async function patchHandler(
+  request: NextApiRequest,
+  response: NextApiResponse,
+) {
+  const username = request.query.username as string;
+  const userInputRequestValues = request.body;
+
+  if (
+    !userInputRequestValues ||
+    Object.keys(userInputRequestValues).length === 0
+  ) {
+    throw new ValidationError({
+      message:
+        "Para realizar esta requisição é necessário enviar pelo menos uma propriedade.",
+      action: "Verifique o corpo desta requisição e tente novamente.",
+    });
+  }
+
+  const updatedUser = await user.update(username, userInputRequestValues);
+  return response.status(200).json(updatedUser);
 }
