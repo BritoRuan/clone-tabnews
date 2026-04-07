1c2556717a95d1b8003db588b018970956576e60
BritoRuan <ruan.desenvolvedor@gmail.com>
2026-04-06 22:18:16 -0300

    feat: add user and session retrieval functions with error handling

--- models/schemas/session/session.ts
+++ models/schemas/session/session.ts
@@ -1,5 +1,6 @@
 import crypto from "node:crypto";
 import database from "@/infra/database/database";
+import { UnauthorizedError } from "@/infra/errors/UnauthorizedError";
 
 const RANDOM_BYTES_LENGTH = 48;
 const EXPIRATION_IN_MILLISECONDS = 60 * 60 * 24 * 30 * 1000; // 30 Days
@@ -36,8 +37,41 @@ async function generateToken(bytesLength: number) {
   return crypto.randomBytes(bytesLength).toString("hex");
 }
 
+async function findOneValidByToken(sessionToken: string) {
+  const sessionFound = await runSelectQuery(sessionToken);
+
+  return sessionFound;
+
+  async function runSelectQuery(sessionToken: string) {
+    const results = await database.query({
+      text: `
+      SELECT
+        *
+      FROM
+        sessions
+      WHERE
+        token = $1
+        AND expires_at > NOW()
+      LIMIT
+        1
+      ;`,
+      values: [sessionToken],
+    });
+
+    if (results.rowCount === 0) {
+      throw new UnauthorizedError({
+        message: "Usuário não possui sessão ativa",
+        action: "Verifique se este usuário está logado e tente novamente.",
+      });
+    }
+
+    return results.rows[0];
+  }
+}
+
 const session = {
   create,
+  findOneValidByToken,
   EXPIRATION_IN_MILLISECONDS,
 };
 
