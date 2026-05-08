43c1b281349a00415c9c5a72079edb180b483337
BritoRuan <ruan.desenvolvedor@gmail.com>
2026-05-08 00:01:18 -0300

    feat: add activation email sending functionality after user creation

--- pages/api/v1/users/index.ts
+++ pages/api/v1/users/index.ts
@@ -2,6 +2,7 @@ import { createRouter } from "next-connect";
 import { NextApiRequest, NextApiResponse } from "next";
 import controller from "@/infra/controllers/controllers";
 import user from "@/models/schemas/users/user";
+import activation from "@/models/schemas/activation/activation";
 
 const router = createRouter<NextApiRequest, NextApiResponse>();
 router.post(postHandler);
@@ -11,5 +12,9 @@ export default router.handler(controller.errorHandlers);
 async function postHandler(request: NextApiRequest, response: NextApiResponse) {
   const userInputValues = request.body;
   const newUser = await user.create(userInputValues);
+
+  // 1. Criar o Token de Ativação
+  await activation.sendEmailToUser(newUser);
+
   return response.status(201).json(newUser);
 }
